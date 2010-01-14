b3a514b33f5947b96739a553d4a2d22f987da01e
Richard Crowley <r@rcrowley.org>
2010-01-14 06:01:45 +0000

    Correct naive implementation that doesn't yet handle updating existing keys.

--- countd/permanent/naive.py
+++ countd/permanent/naive.py
@@ -14,7 +14,7 @@ class Keyspace(object):
     FORMAT = "<q%dc" % settings.KEYSPACE
 
     READ = os.O_RDONLY
-    WRITE = os.O_WRONLY | os.O_CREAT
+    WRITE = os.O_RDWR | os.O_CREAT
 
     def __init__(self, keyspace, flags):
         try:
@@ -30,6 +30,15 @@ class Keyspace(object):
         if hasattr(self, "fd"):
             os.close(self.fd)
 
+    def _pack(self, key, increment):
+        chars = list(key)
+        chars.extend("\0" * (settings.KEY - len(key)))
+        return struct.pack(self.FORMAT, increment, *chars)
+
+    def _unpack(self, buf):
+        parts = struct.unpack(self.FORMAT, buf)
+        return "".join(parts[1:settings.KEY]).strip("\x00"), parts[0]
+
     def range(self, offset, length):
         """
         Yields keys and counts from the keyspace starting at offset and
@@ -42,39 +51,49 @@ class Keyspace(object):
         """
         Increment key's count and resort the keyspace on disk.
         """
-        # FIXME Use os.fcntl to lock regions of the file, otherwise this
-        # is dangerous.
-
-        # Update a key that's already present.
-        offset = self.index.find(key)
-        if offset is not None:
-            return True
-
-        # Place a new key whose count falls somewhere in the middle of the
-        # file.
-        offset = self.deltas.find(increment)
-        if offset is not None:
-            return True
-
-        # Place a new key whose count falls at the end of the file.
         try:
-            os.lseek(self.fd, 0, os.SEEK_END)
-            if self.LENGTH != os.write(self.fd, self._pack(key, increment)):
+
+            # Update a key that's already present.
+            offset = self.index.find(key)
+            if offset is not None:
+                # TODO
+                return self._update(key, increment)
+
+            # Place a new key.
+            if not self._update(key, increment):
                 return False
+
             if fsync:
                 os.fsync(self.fd)
             return True
+
         except OSError:
             return False
 
-    def _pack(self, key, increment):
-        chars = list(key)
-        chars.extend("\0" * (settings.KEY - len(key)))
-        return struct.pack(self.FORMAT, increment, *chars)
+    def _update(self, key, count, offset=0):
+        # FIXME Use os.fcntl to lock regions of the file, otherwise this
+        # is dangerous to do in multiple processes.
 
-    def _unpack(self, buf):
-        parts = struct.unpack(self.FORMAT, buf)
-        return "".join(parts[1:settings.KEY]).strip("\x00"), parts[0]
+        # Place a new whose count falls somewhere in the middle of the file.
+        offset = self.deltas.find(count, offset)
+        if offset is not None:
+            os.lseek(self.fd, offset, os.SEEK_SET)
+            buf = os.read(self.fd, self.LENGTH)
+            if self.LENGTH != len(buf):
+                return False
+            key2, count2 = self._unpack(buf)
+            if not self._update(key2, count2, offset + self.LENGTH):
+                return False
+            os.lseek(self.fd, offset, os.SEEK_SET)
+
+        # Place a key whose count falls at the end of the file.
+        else:
+            offset = os.lseek(self.fd, 0, os.SEEK_END)
+
+        if self.LENGTH != os.write(self.fd, self._pack(key, count)):
+            return False
+        self.deltas.update(count, offset + self.LENGTH)
+        return True
 
     # Make Keyspace objects iterable to make reading easier.
     def __iter__(self):
@@ -109,17 +128,25 @@ class Index(object):
 
 class Deltas(object):
     """
+    A (currently fictional) index for quickly finding the offset at which
+    a key with the given count should be placed.
     """
 
     def __init__(self, keyspace):
-        self.pathname = "%s/%s/deltas" % (settings.DIRNAME, keyspace)
+        self.fd = os.open("%s/%s/keyspace" % (settings.DIRNAME, keyspace),
+            os.O_RDONLY)
 
-    def find(self, count):
+    def _unpack(self, buf):
+        return struct.unpack(Keyspace.FORMAT, buf)[0]
+
+    def find(self, count, offset=0):
         """
-        Return the offset at which the last key with the given count can
-        be found.
+        Return the offset at which a key with the given count should be
+        placed.  Start the search from the given offset.
         """
-        return None
+        for c in self.iter(offset):
+            if c < count:
+                return os.lseek(self.fd, 0, os.SEEK_CUR) - Keyspace.LENGTH
 
     def update(self, count, offset):
         """
@@ -127,3 +154,15 @@ class Deltas(object):
         be found.
         """
         pass
+
+    # Make Deltas objects iterable to make reading easier.
+    def __iter__(self):
+        return self.iter(0)
+    def iter(self, offset):
+        os.lseek(self.fd, offset, os.SEEK_SET)
+        return self
+    def next(self):
+        buf = os.read(self.fd, Keyspace.LENGTH)
+        if Keyspace.LENGTH != len(buf):
+            raise StopIteration()
+        return self._unpack(buf)
